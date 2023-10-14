b088c054090a3fe0f4ab32d523ae0daa8a18cfe2
neowang725 <neowang725@gmail.com>
2023-10-14 12:17:21 +0530

    Time: 1 ms, Memory: 44.7 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -9,38 +9,59 @@
 //     }
 // }
 
-class Solution{
-    public int[] sortedSquares(int[] nums){
-        
-        int idx = nums.length;
-        for(int i=0 ;i<nums.length; i++){
-            if(nums[i]>=0){
-                idx = i;
-                break;
-            }
-        }
-        int left =idx-1, right=idx;
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
         int[] arr = new int[nums.length];
-        int k=0;
-        while(left>=0 && right<nums.length){
-            nums[left] = Math.abs(nums[left]);
-            if(nums[left]>nums[right]){
-                arr[k++] = nums[right]*nums[right];
-                right++;
+        for(int i = nums.length-1;i>=0;i--)
+        {
+            if(Math.abs(nums[left])>Math.abs(nums[right])){
+                arr[i] = nums[left]*nums[left];
+                left++;
             }
             else{
-                arr[k++] = nums[left]*nums[left];
-                left--;
+                arr[i] = nums[right]*nums[right];
+                right--;
             }
-        }
-        while(left>=0){
-            arr[k++]=nums[left]*nums[left];
-            left--;
-        }
-        while(right<nums.length){
-            arr[k++]=nums[right]*nums[right];
-            right++;
         }
         return arr;
     }
 }
+
+
+// class Solution{
+//     public int[] sortedSquares(int[] nums){
+        
+//         int idx = nums.length;
+//         for(int i=0 ;i<nums.length; i++){
+//             if(nums[i]>=0){
+//                 idx = i;
+//                 break;
+//             }
+//         }
+//         int left =idx-1, right=idx;
+//         int[] arr = new int[nums.length];
+//         int k=0;
+//         while(left>=0 && right<nums.length){
+//             nums[left] = Math.abs(nums[left]);
+//             if(nums[left]>nums[right]){
+//                 arr[k++] = nums[right]*nums[right];
+//                 right++;
+//             }
+//             else{
+//                 arr[k++] = nums[left]*nums[left];
+//                 left--;
+//             }
+//         }
+//         while(left>=0){
+//             arr[k++]=nums[left]*nums[left];
+//             left--;
+//         }
+//         while(right<nums.length){
+//             arr[k++]=nums[right]*nums[right];
+//             right++;
+//         }
+//         return arr;
+//     }
+// }
