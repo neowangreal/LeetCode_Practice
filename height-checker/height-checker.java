ae0a5b729990c1c7a45162c7d07ca13bea6e281f
neowang725 <neowang725@gmail.com>
2023-10-14 11:20:39 +0530

    Time: 2 ms, Memory: 40.3 MB - LeetHub

--- height-checker/height-checker.java
+++ height-checker/height-checker.java
@@ -16,11 +16,9 @@ class Solution {
     public int heightChecker(int[] heights) {
         int count = 0;
 
-        // Make a copy of the original heights array to sort it.
         int[] sortedHeights = Arrays.copyOf(heights, heights.length);
         Arrays.sort(sortedHeights);
 
-        // Compare the sorted heights with the original heights.
         for (int i = 0; i < heights.length; i++) {
             if (heights[i] != sortedHeights[i]) {
                 count++;
