5aaeafae44d9e30d1a5659704d64ec967f1dc6a8
neowang725 <neowang725@gmail.com>
2023-11-05 22:45:16 +0530

    Time: 0 ms (100.00%), Space: 56.3 MB (74.64%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getWinner(int[] arr, int k) {
+        int winner = arr[0];
+        int wins = 0;
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (winner > arr[i])
+                wins++;
+            else {
+                wins = 1;
+                winner = arr[i];
+            }
+            
+            if (wins == k)
+                break;
+        }
+        
+        return winner;
+    }
+}
