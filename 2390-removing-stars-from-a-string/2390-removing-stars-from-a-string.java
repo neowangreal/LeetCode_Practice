20f14975101bd2dda54ac6816e09d8d7e1a0e519
neowang725 <neowang725@gmail.com>
2023-11-14 21:26:11 +0530

    Time: 24 ms (94.01%), Space: 44.5 MB (83.56%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,31 +1,31 @@
-// class Solution {
-//         public String removeStars(String s) {
-//         StringBuilder res = new StringBuilder();
-//         for (char c : s.toCharArray())
-//             if (c == '*')
-//                 res.setLength(res.length() - 1);
-//             else
-//                 res.append(c);
-//         return res.toString();
-//     }
-// }
-
-class Solution{
-    public String removeStars(String s){
-        Stack<Character>st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='*'){
-                st.pop();
-            }
-            else{
-                st.push(s.charAt(i));
-            }
-        }
-
-        String ans="";
-        for(char e:st){
-            ans+=e;
-        }
-        return ans;
+class Solution {
+        public String removeStars(String s) {
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray())
+            if (c == '*')
+                res.setLength(res.length() - 1);
+            else
+                res.append(c);
+        return res.toString();
     }
 }
+
+// class Solution{
+//     public String removeStars(String s){
+//         Stack<Character>st=new Stack<>();
+//         for(int i=0;i<s.length();i++){
+//             if(s.charAt(i)=='*'){
+//                 st.pop();
+//             }
+//             else{
+//                 st.push(s.charAt(i));
+//             }
+//         }
+
+//         String ans="";
+//         for(char e:st){
+//             ans+=e;
+//         }
+//         return ans;
+//     }
+// }
