429ae9ffdd8520a6dca2223ff04fd14fcf0885fd
neowang725 <neowang725@gmail.com>
2023-12-07 20:32:49 +0530

    Time: 1 ms (100.00%), Space: 44.8 MB (17.34%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        // String s = "";
+        for(int i=num.length()-1; i>=0; i--){
+            if(num.charAt(i)%2!=0){
+                return num.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+}
