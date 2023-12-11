74d6e5f18adb14da68884af05263ba2ced488457
neowang725 <neowang725@gmail.com>
2023-12-11 12:05:38 +0530

    Time: 11 ms (24.41%), Space: 44.9 MB (5.58%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -2,6 +2,9 @@ class Solution {
     public int findSpecialInteger(int[] arr) {
         int n = arr.length;
         // int i = arr[0];
+        if(n<2){
+            return arr[0];
+        }
         int ans = 0;
         HashMap<Integer, Integer> hm = new HashMap<>();
         
@@ -19,25 +22,3 @@ class Solution {
 }
 
 
-// public class Solution {
-//     public int findSpecialInteger(int[] arr) {
-//         if (arr.length < 2) {
-//             return arr[0];
-//         }
-//         int result = 0;
-//         int percent = (int) (arr.length * 0.25);
-//         Map<Integer, Integer> map = new HashMap<>();
-        
-//         for (int i : arr) {
-//             map.put(i, map.getOrDefault(i, 0) + 1);
-//         }
-
-//         for (int key : map.keySet()) {
-//             if (map.get(key) > percent) {
-//                 result = key;
-//             }
-//         }
-
-//         return result;
-//     }
-// }
