c9531e0c2307942b394a51f4124db19dddd4be81
neowang725 <neowang725@gmail.com>
2023-12-13 10:52:36 +0530

    Time: 2 ms (88.89%), Space: 43.5 MB (83.33%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -1,24 +1,51 @@
+// class Solution {
+//     public int numSpecial(int[][] mat) {
+//         int row  = mat.length;
+//         int col = mat[0].length;
+//         int cnt = 0;
+//         for(int i=0;i<row;i++){
+//             for(int j=0;j<col;j++){
+//                 int sum = 0;
+//                 if(mat[i][j]==1){
+//                     for(int k=0;k<row;k++){
+//                     sum += mat[k][j];
+//                 }
+//                 for(int k=0;k<col;k++){
+//                     sum+=mat[i][k];
+//                 }
+//                 if(sum == 2){
+//                     cnt++;
+//                 }
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// }
+
+
 class Solution {
     public int numSpecial(int[][] mat) {
         int row  = mat.length;
         int col = mat[0].length;
+        int[] n = new int[row];
+        int[] m = new int[col];
         int cnt = 0;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                int sum = 0;
-                if(mat[i][j]==1){
-                    for(int k=0;k<row;k++){
-                    sum += mat[k][j];
-                }
-                for(int k=0;k<col;k++){
-                    sum+=mat[i][k];
-                }
-                if(sum == 2){
+                n[i] += mat[i][j];
+                m[j] += mat[i][j];
+            }
+        }
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(mat[i][j]==1 && n[i]==1 && m[j]==1){
                     cnt++;
-                }
                 }
             }
         }
         return cnt;
     }
 }
+
