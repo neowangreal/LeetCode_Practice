b1d29ca070c25ed85b138bebeb947ae159a616f5
neowang725 <neowang725@gmail.com>
2024-01-16 11:18:02 +0530

    Time: 19 ms (14.15%), Space: 44.7 MB (8.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,34 +1,44 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+
+        for(int i=0;i<t.length();i++){
+            if(map.get(t.charAt(i))!=null){
+                if(map.get(t.charAt(i))==1)
+                  map.remove(t.charAt(i));
+                else
+                  map.put(t.charAt(i),map.get(t.charAt(i))-1);
+            }
+            else{
+                return false;
+            }
+        }
+        return map.isEmpty();
+    }
+}
+
 // class Solution {
 //     public boolean isAnagram(String s, String t) {
 //         int len1 = s.length();
 //         int len2 = t.length();
-//         if(len1!=len2) return false;
-//         int[] arr = new int[26];
-//         for(int i=0;i<len1;i++){
-//             arr[s.charAt(i)-'a']++;
+//         if(len1!=len2){
+//             return false;
 //         }
+//             int[] cnt = new int[26];
+//             for(int i=0;i<len1;i++){
+//                 cnt[s.charAt(i)-'a']++;
+//             }
+//             for(int i=0;i<len2;i++){
+//                 cnt[t.charAt(i)-'a']--;
+//             }
+//             for(int i=0;i<cnt.length;i++){
+//                 if(cnt[i]!=0){
+//                     return false;
+//                 }
+//             }
+//             return true;
 //     }
 // }
-
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        int len1 = s.length();
-        int len2 = t.length();
-        if(len1!=len2){
-            return false;
-        }
-            int[] cnt = new int[26];
-            for(int i=0;i<len1;i++){
-                cnt[s.charAt(i)-'a']++;
-            }
-            for(int i=0;i<len2;i++){
-                cnt[t.charAt(i)-'a']--;
-            }
-            for(int i=0;i<cnt.length;i++){
-                if(cnt[i]!=0){
-                    return false;
-                }
-            }
-            return true;
-    }
-}
