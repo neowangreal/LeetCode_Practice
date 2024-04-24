2d7fd887573d506f5055a9f475cd6ba7016f05e1
neowang725 <neowang725@gmail.com>
2024-04-24 09:18:39 +0530

    Time: 0 ms (100.00%), Space: 40 MB (73.08%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int tribonacci(int n) {
+        int a = 0,b = 1,c = 1;
+
+        while(n > 0){
+            
+            int d = a + b +c;
+            a = b;
+            b = c;
+            c = d;
+            n--;
+        }
+
+        return a;
+    }
+}
